0b26b3c31f59af308d67ad5af30a31a6a5718d2e
nguyentrong101094 <nguyentrong101094@gmail.com>
2022-07-08 15:52:58 +0700

    Add Adjust assembly definition files as package.

---
+++ Editor/SetupMenuItems.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Omnilatent.AdjustUnity.Editor
+{
+    public class SetupMenuItems : MonoBehaviour
+    {
+        [MenuItem("Tools/Omnilatent/Adjust Unity/Import Adjust Assembly Definitions")]
+        public static void ImportNetworkSecurityConfig()
+        {
+            string path = GetPackagePath("Assets/Omnilatent/AdjustWrapper/AdjustAssemblyDefinition.unitypackage", "AdjustAssemblyDefinition");
+            AssetDatabase.ImportPackage(path, true);
+        }
+
+        static string GetPackagePath(string path, string filename)
+        {
+            if (!File.Exists($"{Application.dataPath}/../{path}"))
+            {
+                Debug.Log($"{filename} not found at {path}, attempting to search whole project for {filename}");
+                string[] guids = AssetDatabase.FindAssets($"{filename} l:package");
+                if (guids.Length > 0)
+                {
+                    path = AssetDatabase.GUIDToAssetPath(guids[0]);
+                }
+                else
+                {
+                    Debug.LogError($"{filename} not found at {Application.dataPath}/../{path}");
+                    return null;
+                }
+            }
+            return path;
+        }
+    }
+}
